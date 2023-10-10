758ee0032142f8ce2438f4c8113aa2956b76273c
OlgaZhavrid <OlgaZhavrid@PL-NBW-1165>
2023-10-10 16:57:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[7];
+int[] array2 = new int[7];
+int[] array3 = new int[7];
+
+array[0] = 7;
+array[1] = 3;
+
+array2[0] = 5;
+array2[1] = 5;
+
+array3[0] = 2;
+array3[1] = 8;
+
+Console.WriteLine("1:" + array[0] + " 2:" + array2[4]);
