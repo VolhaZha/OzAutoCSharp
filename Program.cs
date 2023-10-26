f87d0f110b08c03304295ca92865f9fbaa27f6bd
VolhaZha <olgazhauryd@gmail.com>
2023-10-26 13:45:58 +0200

    Main Empty

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-int[] array = new int[7];
-int[] array2 = new int[7];
-int[] array3 = new int[7];
-
-array[0] = 7;
-array[1] = 3;
-
-array2[0] = 5;
-array2[1] = 5;
-
-Console.WriteLine("1:" + array[0] + " 2:" + array2[4]);
+﻿
