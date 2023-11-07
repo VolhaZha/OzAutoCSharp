5004bc9ce0f6293ed0c5b7d839d49c00c30c4553
VolhaZha <olgazhauryd@gmail.com>
2023-11-07 09:06:21 +0100

    Task 2

--- Order.cs
+++ Order.cs
@@ -1,31 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OzTest1
+﻿namespace OzTest1
 {
     internal class Order
     {
-        private string name = "Name";
+        private string name;
         private long phone;
         private float price;
-        private string address = "Address";
+        private string address;
 
         public string Name { get { return name; } set { name = value; } }
-        public long Phone { get { return phone; } set { phone = value; } }
-        public float Price { get { return price; } set { price = value; } }
+        public long Phone { get { return phone; } set { phone = value;
+                string phoneString = value.ToString();             
+                if (phoneString.Length != 13)
+                {
+                  throw new Exception("Invalid Phone");
+                }
+            }
+        }
+        public float Price { get { return price; } set { price = value;
+                if (value <= 0 || value > 1000)
+                {
+                    throw new Exception("Invalid Price");
+                }
+            }
+        }
         public string Address { get { return address; } set { address = value; } }
 
         public Order(string name, long phone, float price, string address)
         {
             Name = name;
-            Phone = phone;
-            Price = price;
+            try
+            {
+                Phone = phone;
+            }           
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occurred: {ex.Message} - for {name}");
+            }
+            try
+            {
+                Price = price;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occurred: {ex.Message} - for {name}");
+            }
             Address = address;
         }
-
         public override string ToString()
         {
             return $"Product: {Name}, Order Number: {Phone}, Price: {Price}, Destination: {Address}";
--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using OzTest1;
 
-Order order1 = new Order("book", 333444555, 10, "Minsk");
-Order order2 = new Order("food", 333444777, 20, "Wro");
-Order order3 = new Order("sport", 333444888, 30, "Ber");
+Order order1 = new Order("Whyskey", 333444555, 10, "Minsk");
+Order order2 = new Order("Food", 333444777, 20, "Wro");
+Order order3 = new Order("Sport", 375444888, 3000, "Ber");
 
 Order[] orderGeneral = { order1, order2, order3 };
 
 foreach (Order order in orderGeneral)
 {
-    Console.WriteLine(order);
+    string phoneString = order.Phone.ToString();
+    
+    if (phoneString.StartsWith("375"))
+    {
+        Console.WriteLine($"Orders with phone starting from 375: {order}");
+    }
+    
+    if (order.Price <= 20 & order.Name.StartsWith ("Whys"))
+    {
+        Console.WriteLine($"Orders with Price not more then 20 and Name starting as Whys: {order}");
+    }
 }
