b639340c3092f3d137a458e225a0fca6952dccf1
VolhaZha <olgazhauryd@gmail.com>
2023-11-26 20:11:09 +0100

    Task2_26Nov_1

--- Order.cs
+++ Order.cs
@@ -2,50 +2,49 @@
 {
     internal class Order
     {
-        private string name;
-        private long phone;
-        private float price;
-        private string address;
+        private string _name;
+        private long _phone;
+        private float _price;
+        private string _address;
 
-        public string Name { get { return name; } set { name = value; } }
-        public long Phone { get { return phone; } set { phone = value;
+        const long PHONE_LENGTH = 13;
+        const float PRICE_LOW = 0;
+        const float PRICE_HIGH = 1000;
+
+        public string Name { get; set; }
+        public long Phone { 
+            get { return _phone; } 
+            set 
+            { 
+                _phone = value;
                 string phoneString = value.ToString();             
-                if (phoneString.Length != 13)
+                if (phoneString.Length != PHONE_LENGTH)
                 {
                   throw new Exception("Invalid Phone");
                 }
             }
         }
-        public float Price { get { return price; } set { price = value;
-                if (value <= 0 || value > 1000)
+        public float Price { 
+            get { return _price; } 
+            set 
+            { 
+                _price = value;
+                if (value <= PRICE_LOW || value > PRICE_HIGH)
                 {
                     throw new Exception("Invalid Price");
                 }
             }
         }
-        public string Address { get { return address; } set { address = value; } }
+        public string Address { get; set; }
 
         public Order(string name, long phone, float price, string address)
         {
             Name = name;
-            try
-            {
-                Phone = phone;
-            }           
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception occurred: {ex.Message} - for {name}");
-            }
-            try
-            {
-                Price = price;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception occurred: {ex.Message} - for {name}");
-            }
+            Phone = phone;
+            Price = price;
             Address = address;
         }
+
         public override string ToString()
         {
             return $"Product: {Name}, Order Number: {Phone}, Price: {Price}, Destination: {Address}";
--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using OzTest1;
 
-Order order1 = new Order("Whyskey", 333444555, 10, "Minsk");
-Order order2 = new Order("Food", 333444777, 20, "Wro");
-Order order3 = new Order("Sport", 375444888, 3000, "Ber");
+Order order1 = new Order("Whyskey", 3334445551112, 10, "Minsk");
+Order order2 = new Order("Food", 3334447772212, 20, "Wro");
+Order order3 = new Order("Sport", 3754448882212, 300, "Ber");
 
 Order[] orderGeneral = { order1, order2, order3 };
 
@@ -18,5 +18,18 @@ foreach (Order order in orderGeneral)
     if (order.Price <= 20 & order.Name.StartsWith ("Whys"))
     {
         Console.WriteLine($"Orders with Price not more then 20 and Name starting as Whys: {order}");
+    }
+}
+
+static Order CreateOrder(string name, long phone, float price, string address)
+{
+    try
+    {
+        return new Order(name, phone, price, address);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception occurred: {ex.Message} - for {name}");
+        return null;
     }
 }
