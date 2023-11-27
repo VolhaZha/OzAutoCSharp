cb406575a631f4c18c8754a90b28fe6a648e64f7
VolhaZha <olgazhauryd@gmail.com>
2023-11-27 11:32:35 +0100

    Task2_27Nov

--- Order.cs
+++ Order.cs
@@ -2,14 +2,41 @@
 {
     internal class Order
     {
-        private string _name = "Name";
+        private string _name;
         private long _phone;
         private float _price;
-        private string _address = "Address";
+        private string _address;
 
-        public string Name { get; set;}
-        public long Phone { get; set; }
-        public float Price { get; set; }
+        const long PHONE_LENGTH = 13;
+        const float PRICE_LOW = 0;
+        const float PRICE_HIGH = 1000;
+
+        public string Name { get; set; }
+        public long Phone
+        {
+            get { return _phone; }
+            set
+            {
+                _phone = value;
+                string phoneString = value.ToString();
+                if (phoneString.Length != PHONE_LENGTH)
+                {
+                    throw new Exception("Invalid Phone");
+                }
+            }
+        }
+        public float Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                if (value <= PRICE_LOW || value > PRICE_HIGH)
+                {
+                    throw new Exception("Invalid Price");
+                }
+            }
+        }
         public string Address { get; set; }
 
         public Order(string name, long phone, float price, string address)
