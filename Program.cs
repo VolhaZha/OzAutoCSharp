043728a217d0a9e071b9cc208c90e1f5c1e34bee
VolhaZha <olgazhauryd@gmail.com>
2024-01-10 15:53:20 +0100

    Task4

--- Program.cs
+++ Program.cs
@@ -1,5 +1,41 @@
 using OzTest1;
 
+//List Remove 1
+var people = new List<string>() { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam", "Tom", "Alice" };
+people.RemoveAt(0);
+Console.WriteLine($"Listpeople: {people.Count}");
+
+//List Add To head
+MyList<int> list = new MyList<int>();
+list.AddToHead(new ListNode<int>(1));
+list.AddToHead(new ListNode<int>(3));
+list.AddToHead(new ListNode<int>(7));
+list.AddToHead(new ListNode<int>(8));
+
+//List Remove 2
+list.Remove(9);
+list.Remove(0);
+Console.WriteLine($"List: {list.GetCount()}");
+
+//List Get Element At
+var elementByIndex = list.GetElementAt(1);
+
+//List Count
+int count = list.GetCount();
+Console.WriteLine($"Count: {count}");
+
+//List Contains
+int index = list.Contains(3);
+if (index != -1)
+{
+    Console.WriteLine($"Number found at index {index}");
+}
+else
+{
+    Console.WriteLine("Number not found in the list");
+}
+
+//Orders Arrays
 Order order1 = new Order("Whyskey", 3334445551112, 10, "Minsk");
 Order order2 = new Order("Food", 3334447772212, 20, "Wro");
 Order order3 = new Order("Sport", 3754448882212, 300, "Ber");
@@ -26,4 +62,25 @@ static Order CreateOrder(string name, long phone, float price, string address)
         Console.WriteLine($"Exception occurred: {ex.Message} - for {name}");
         return null;
     }
+}
+
+//List Of Order Items
+MyList<Order> listOrder = new MyList<Order>();
+listOrder.AddToHead(new ListNode<Order>(order1));
+listOrder.AddToHead(new ListNode<Order>(order2));
+listOrder.AddToHead(new ListNode<Order>(order3));
+listOrder.AddToHead(new ListNode<Order>(order4));
+Console.WriteLine("\nList Of Order Items:");
+ListNode<Order>? currentOrderNode = listOrder.Head;
+while (currentOrderNode != null)
+{
+    Console.WriteLine($"Order: {currentOrderNode.data}");
+    currentOrderNode = currentOrderNode.nextNode;
+}
+
+//List Of Order Items via iEnumerable
+Console.WriteLine("\nList Of Order Items iEnumerable:");
+foreach (var order in listOrder)
+{
+    Console.WriteLine($"Order: {order}");
 }
